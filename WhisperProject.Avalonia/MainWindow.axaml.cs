1e59d9873474419a73515351ca1411a421b9503f
fmam0126 <fmam012026@gmail.com>
2026-08-03 09:10:53 +0200

    add avalonia project and rename core project

---
+++ WhisperProject.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace WhisperProject.Avalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
